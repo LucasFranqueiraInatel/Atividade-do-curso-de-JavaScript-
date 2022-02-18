309829ac66231e67153ee203a12ab6feb6e4148d
Lucas Franqueira Carneiro Fonseca <lucas.f@gec.inatel.br>
2022-02-18 20:42:13 -0300

    .

---
+++ 01-Fundamentals-Part-1/assignments/data_types/script.js
@@ -0,0 +1,11 @@
+let country = "Brazil";
+let continent = "America Latina";
+let population = 200;
+let isIsland = false;
+const language;
+
+console.log("My country is " + country);
+console.log('My continent is ' + continent);
+console.log('The population of my country is ' + population + ' millions');
+console.log(isIsland);
+console.log();
---
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -0,0 +1,7 @@
+let country = "Brazil";
+let continent = "America Latina";
+let population = 200;
+
+console.log("My country is " + country);
+console.log('My continent is ' + continent);
+console.log('The population of my country is ' + population + ' millions');
