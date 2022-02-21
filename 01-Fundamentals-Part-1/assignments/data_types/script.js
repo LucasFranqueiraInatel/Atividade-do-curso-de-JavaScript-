9b20042af841ee19eff59823fd9ee907b1d151ba
Lucas Franqueira Carneiro Fonseca <lucas.f@gec.inatel.br>
2022-02-21 01:12:58 -0300

    webSite da BalleBot da Rafaella Ballerin

--- 01-Fundamentals-Part-1/assignments/data_types/script.js
+++ 01-Fundamentals-Part-1/assignments/data_types/script.js
@@ -2,10 +2,10 @@ let country = "Brazil";
 let continent = "America Latina";
 let population = 200;
 let isIsland = false;
-const language;
+const language = "Portugues";
 
 console.log("My country is " + country);
 console.log('My continent is ' + continent);
 console.log('The population of my country is ' + population + ' millions');
 console.log(isIsland);
-console.log();
+console.log("The language that my country speak is " + language);
